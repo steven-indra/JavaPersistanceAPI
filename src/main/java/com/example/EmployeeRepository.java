39574df62e8e26cfc5b3bb65bfd743a4b16c6841
trainee <trainee>
2017-04-20 08:49:20 +0700

    experimenting

--- src/main/java/com/example/EmployeeRepository.java
+++ src/main/java/com/example/EmployeeRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee,Long> {
     public List<Employee> findByLastName(@Param("lastName") String lastName);
+//    public List<Employee> findByFirstNameAndLastNameContaining(@Param("contain") String contain);
+    public List<Employee> findByLastNameContainingOrderByLastNameAsc(@Param("lastName") String lastName);
 }
